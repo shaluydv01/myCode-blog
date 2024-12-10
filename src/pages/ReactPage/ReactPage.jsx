1bf355560c2c74e89d2a6023124233fcca8d5048
shaluydv01 <ydvshalu420@gmail.com>
2024-12-10 15:41:44 +0530

    First-commit

--- src/pages/ReactPage/ReactPage.jsx
+++ src/pages/ReactPage/ReactPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./ReactPage.css";
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-import { note1, reactIntro, reactPara2, reactPara3, reactPara4 } from "../../../courseContent/ReactPage.js";
+import { note1, reactIntro, reactPara2, reactPara3, reactPara4 } from "./courseContent/ReactPage";
 
 
 const ReactPage = () => {
